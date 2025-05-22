e7c63815b9ffe17c2818320ae23c60c8ab42a479
lone2307 <dungnghiem2307@gmail.com>
2025-05-22 10:05:48 +0700

    testing chessboard

--- game_rule.js
+++ game_rule.js
@@ -1,3 +1,5 @@
+import { Chess } from 'node_modules/chess.js/dist/esm/chess.js';
+
 var board = null
 var game = new Chess()
 
@@ -9,14 +11,22 @@ function onDragStart (source, piece, position, orientation) {
   if (piece.search(/^b/) !== -1) return false
 }
 
-function makeRandomMove () {
+function blackMove () {
   var possibleMoves = game.moves()
 
   // game over
   if (possibleMoves.length === 0) return
+    let best_move = 0
+    let best_score = Infinity
+    for (let x = 0; x < possibleMoves.length; x++){
+        let score = minimax(possibleMoves[x], 3, false, board)
+        if (score < best_score){
+            best_move = x
+            best_score = score
+        }
+    }
 
-  var randomIdx = Math.floor(Math.random() * possibleMoves.length)
-  game.move(possibleMoves[randomIdx])
+  game.move(possibleMoves[best_move])
   board.position(game.fen())
 }
 
@@ -32,7 +42,7 @@ function onDrop (source, target) {
   if (move === null) return 'snapback'
 
   // make random legal move for black
-  window.setTimeout(makeRandomMove, 250)
+  window.setTimeout(blackMove, 250)
 }
 
 // update the board position after the piece snap
@@ -48,4 +58,111 @@ var config = {
   onDrop: onDrop,
   onSnapEnd: onSnapEnd
 }
-board = Chessboard('myBoard', config)
+
+function eval_score(game) {
+    const value = { "r": 50, "n": 32, "b": 33, "q": 90, "p": 10, "k": 0 }
+    const eval_table = {
+        "p": [
+            0, 0, 0, 0, 0, 0, 0, 0,
+            50, 50, 50, 50, 50, 50, 50, 50,
+            10, 10, 20, 30, 30, 20, 10, 10,
+            5, 5, 10, 25, 25, 10, 5, 5,
+            0, 0, 0, 20, 20, 0, 0, 0,
+            5, -5, -10, 0, 0, -10, -5, 5,
+            5, 10, 10, -20, -20, 10, 10, 5,
+            0, 0, 0, 0, 0, 0, 0, 0
+        ],
+        "n": [
+            -50, -40, -30, -30, -30, -30, -40, -50,
+            -40, -20, 0, 0, 0, 0, -20, -40,
+            -30, 0, 10, 15, 15, 10, 0, -30,
+            -30, 5, 15, 20, 20, 15, 5, -30,
+            -30, 0, 15, 20, 20, 15, 0, -30,
+            -30, 5, 10, 15, 15, 10, 5, -30,
+            -40, -20, 0, 5, 5, 0, -20, -40,
+            -50, -40, -30, -30, -30, -30, -40, -50,
+        ],
+        "b": [
+            -20, -10, -10, -10, -10, -10, -10, -20,
+            -10, 0, 0, 0, 0, 0, 0, -10,
+            -10, 0, 5, 10, 10, 5, 0, -10,
+            -10, 5, 5, 10, 10, 5, 5, -10,
+            -10, 0, 10, 10, 10, 10, 0, -10,
+            -10, 10, 10, 10, 10, 10, 10, -10,
+            -10, 5, 0, 0, 0, 0, 5, -10,
+            -20, -10, -10, -10, -10, -10, -10, -20,
+        ],
+        "r": [
+            0, 0, 0, 0, 0, 0, 0, 0,
+            5, 10, 10, 10, 10, 10, 10, 5,
+            -5, 0, 0, 0, 0, 0, 0, -5,
+            -5, 0, 0, 0, 0, 0, 0, -5,
+            -5, 0, 0, 0, 0, 0, 0, -5,
+            -5, 0, 0, 0, 0, 0, 0, -5,
+            -5, 0, 0, 0, 0, 0, 0, -5,
+            0, 0, 0, 5, 5, 0, 0, 0
+        ],
+        "q": [
+            -20, -10, -10, -5, -5, -10, -10, -20,
+            -10, 0, 0, 0, 0, 0, 0, -10,
+            -10, 0, 5, 5, 5, 5, 0, -10,
+            -5, 0, 5, 5, 5, 5, 0, -5,
+            0, 0, 5, 5, 5, 5, 0, -5,
+            -10, 5, 5, 5, 5, 5, 0, -10,
+            -10, 0, 5, 0, 0, 0, 0, -10,
+            -20, -10, -10, -5, -5, -10, -10, -20
+        ],
+        "k": [
+            -30, -40, -40, -50, -50, -40, -40, -30,
+            -30, -40, -40, -50, -50, -40, -40, -30,
+            -30, -40, -40, -50, -50, -40, -40, -30,
+            -30, -40, -40, -50, -50, -40, -40, -30,
+            -20, -30, -30, -40, -40, -30, -30, -20,
+            -10, -20, -20, -20, -20, -20, -20, -10,
+            20, 20, 0, 0, 0, 0, 20, 20,
+            20, 30, 10, 0, 0, 10, 30, 20
+        ]
+    }
+
+    let score = 0
+    const cur_game = game.board()
+    for (let x = 0; x < cur_game.length; x++) {
+        for (let y = 0; y < cur_game[x].length; y++) {
+            const piece = cur_game[x][y]
+            if (piece) {
+                score += piece.color === 'w' ? eval_table[piece.type][8 * (x + 1) + y] + value[piece.type] : -eval_table[piece.type][8 * (7 - x + 1) + (7 - y)] - value[piece.type]
+            }
+        }
+    }
+    return score
+}
+
+function minimax(move, depth, maxPlayer, board, alpha = -Infinity, beta = Infinity) {
+    if (depth === 0 | board.isGameOver()) {
+        return eval_score(board)
+    } else if (maxPlayer) {
+        let max = -Infinity
+        for (const pos_move of board.moves()) {
+            const newGame = new Chess(board.fen());
+            newGame.move(pos_move)
+            const value = minimax(move, depth - 1, !maxPlayer, newGame, alpha, beta)
+            max = Math.max(max, value)
+            alpha = Math.max(alpha, max)
+            if (beta <= alpha) break
+        }
+        return max
+    } else {
+        let min = Infinity
+        for (const pos_move of board.moves()) {
+            const newGame = new Chess(board.fen());
+            newGame.move(pos_move)
+            const value = minimax(move, depth - 1, !maxPlayer, newGame, alpha, beta)
+            min = Math.min(min, value)
+            beta = Math.min(beta, min)
+            if (beta <= alpha) break
+        }
+        return min
+    }
+}
+
+board = Chessboard("myBoard", config)
